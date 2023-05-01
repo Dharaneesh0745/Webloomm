3d1c1c97339e99979d8c7dfbb72c2510ee3b6f0a
Dharaneesh Boobalan <dharaneesh0745@gmail.com>
2023-05-01 17:21:58 +0530

    first commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <h1 className="d-flex justify-content-center mt-5">Hello, Succesfully Logged in...</h1>;
+};
+
+export default Dashboard;
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Styles from "./page.css";
+
+const HomePage = () => {
+  return <Styles>
+  <div className="head">Welcome to Dharaneesh's Next Login App</div>; </Styles> 
+};
+
+export default HomePage;
